310aa1a8bdba34193110d3cc52d9ee5efaf8e98e
Vishwas Srivastava <visrivas@microsoft.com>
2023-09-14 15:15:25 +0530

    changes related to home screen

--- components/navigation/HomeScreen/HomeScreen.jsx
+++ components/navigation/HomeScreen/HomeScreen.jsx
@@ -1,13 +1,115 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import styles from './HomeScreen.style'
+import {
+    View,
+    Text,
+    TextInput,
+    FlatList,
+    TouchableOpacity,
+    Modal,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ListItem } from 'react-native-elements'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const HomeScreen = () => {
+
+    const [user, setUser] = useState();
+    const [myTrips, setMyTrips] = useState([]);
+
+    useEffect(() => {
+
+        //var name = async () => await AsyncStorage.getItem('givenName')
+        setUser('Vishwas')
+
+    }, []);
+
+    const trips = [
+        {
+            from: 'Microsoft',
+            to: 'Airport',
+            travelDate: new Date(),
+            notifications: [
+                {
+                    name: 'Amy Farha',
+                    status: 'Pending',
+                    type: 'car'
+
+                },
+                {
+                    name: 'Amy Farha',
+                    status: 'Pending',
+                    type: 'car'
+
+                }
+            ]
+        },
+        {
+            from: 'Apple',
+            to: 'Airport',
+            travelDate: new Date(),
+            notifications: [
+                {
+                    name: 'Amy Farha',
+                    status: 'Pending',
+                    type: 'car'
+                },
+                {
+                    name: 'Amy Farha',
+                    status: 'Pending',
+                    type: 'car'
+
+                }
+            ]
+        }
+    ];
+
+    notificationRender = ({ item }) => (
+        <View style={styles.tripRowStyle}>
+            {
+            item.type == 'car' 
+                  ? <Ionicons name={'car'} size={20} width='10%' marginleft='3%' marginTop='1%' />   
+                  : null
+            }
+            <Text style={styles.tripColumnStyle}>{item.name}</Text>
+            <Text style={styles.tripColumnStyle}>{item.status}</Text>
+            <Ionicons name={'thumbs-up-outline'} size={20} width='10%' marginleft='1%' marginTop='1%' />
+        </View>
+    );
+
+    tripRender = ({ item }) => (
+        <View style={styles.tripCardContainer}>
+            <Text style={styles.tripHeader}>{item.from} to {item.to}</Text>
+
+            <FlatList
+                keyExtractor={this.keyExtractor}
+                data={item.notifications}
+                renderItem={this.notificationRender}
+            />
+            
+        </View>
+    );
+
+    keyExtractor = (item, index) => index.toString()
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Profile')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+
+        <View style={styles.container}>
+
+            <View style={styles.welcomeMessageContainer}>
+                <Text style={styles.welcomeMessage}>Welcome {user}!</Text>
+            </View>
+
+            <View style={styles.flatlistContainer} >
+                <FlatList
+                    keyExtractor={this.keyExtractor}
+                    data={trips}
+                    renderItem={this.tripRender}
+                />
+            </View>
+
         </View>
     )
 }
--- components/navigation/HomeScreen/HomeScreen.style.js
+++ components/navigation/HomeScreen/HomeScreen.style.js
@@ -1,13 +1,15 @@
 import { StyleSheet } from "react-native";
-
 import { COLORS, FONT, SIZES } from "../../../constants";
 
 const styles = StyleSheet.create({
     container: {
-        width: "100%",
-        height: "100%",
-        backgroundColor: '#fff',
-
+        flex:1,
+        flexDirection:'colum',
+        shadowColor: 'pink',
+        shadowOffset: { width: -2, height: 4 },
+        shadowOpacity: 0.2,
+        backgroundColor: 'white',
+        shadowRadius: 3,
     },
     searchContainer: {
         flex: 1,
@@ -36,17 +38,72 @@ const styles = StyleSheet.create({
         height: "100%",
         width: "70%",
     },
+    flatlistContainer:{
+        flex:12,
+        backgroundColor:'black'
+    },
     searchInput: {
         fontFamily: FONT.regular,
         width: "100%",
         height: "100%",
         paddingHorizontal: SIZES.xSmall,
     },
+    welcomeMessageContainer: {
+        flex:1,
+        marginTop: "2%",
+        flexDirection:'row',
+        justifyContent:"flex-start",
+        //backgroundColor: '#e9e9ec',
+        backgroundColor: 'yellow',
+        marginLeft: 4,
+        //marginBottom: 5,
+        paddingBottom: 10,
+        borderRadius: SIZES.xSmall,
+    },
     welcomeMessage: {
         fontFamily: FONT.bold,
         fontSize: SIZES.xLarge,
         color: COLORS.primary,
-        marginTop: 2,
+        marginTop: "2%",
+        marginLeft: "2%"
+    },
+    tripCardContainer: {
+        width: "98%",
+        height: "100%",
+        marginTop: "2%",
+        //backgroundColor: '#e9e9ec',
+        backgroundColor: 'red',
+        marginLeft: 4,
+        //marginBottom: 5,
+        paddingBottom: 10,
+        flex: 1,
+        shadowColor: '#171717',
+        shadowOffset: { width: -2, height: 4 },
+        shadowOpacity: 1,
+        shadowRadius: 3,
+        borderRadius: SIZES.xSmall,
+    },
+    tripHeader: {
+        fontFamily: FONT.bold,
+        fontSize: SIZES.medium,
+        color: COLORS.primary,
+        marginTop: "1%",
+        marginLeft: "1%",
+    },
+    tripRowStyle: {
+        backgroundColor: COLORS.red,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: "1%",
+    },
+    tripColumnStyle: {
+        backgroundColor: COLORS.yellow,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        height: "100%",
+        marginLeft: "5%",
     },
 });
 
